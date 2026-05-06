81f6f00392a384a2045c93061b25ca892c3a8fd2
Jupyter Backend <jupyter-backend@local>
2026-05-06 13:29:07 +0000

    Initial commit

---
+++ P2/Knoten.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+
+class Knoten:
+    def __init__(self):
+        pass
+
+    def expand(self) -> list[Knoten]:
+        pass
---
+++ P3/Suche.py
@@ -0,0 +1,11 @@
+from Backend.Student_Files.P3 import Knoten
+
+
+class Suche:
+
+    def starte_Suchalgorithmus(self, node: Knoten) -> Knoten:
+        """
+        node: Startknoten
+        @return: Zielknoten, oder Fehler (falls kein Zielknoten gefunden)
+        """
+        pass
