d28d94e9b87807cf5528480a10c91f9406d42138
HenrikAbtmeyer28 <flourixmk@gmail.com>
2026-05-06 15:01:44 +0000

    Erste Aufgabe, er dreht sich im Kreis

--- PacmanAgent.py
+++ PacmanAgent.py
@@ -1,4 +1,5 @@
 from game_core.Pacman_Environment import Pacman_Environment
+from game_core.config import TILE_TYPES
 
 """
     0: 'left',
@@ -34,11 +35,41 @@ class PacmanAgent:
 
         if self.is_running and not (self.terminated or self.truncated):
             # TODO P1
-            self.action = 1
-            observation, reward, self.terminated, self.truncated, self.statistics = self.env.step(self.action)
+            if self.isNextFree(self.action):
+                print("Next ist free, Step wird gemacht")
+                observation, reward, self.terminated, self.truncated, self.statistics = self.env.step(self.action)
+            else:
+                print("Turn Right")
+                self.turnRight()
+                if self.isNextFree(self.action):
+                    observation, reward, self.terminated, self.truncated, self.statistics = self.env.step(self.action)
             if self.terminated or self.truncated:
                 self.is_last_step = True
         return observation, self.is_last_step, self.statistics
+
+    def isNextFree(self, action):
+        map = self.env.view
+        row, column = self.env.pacman.get_position()
+        match action:
+            case 0:
+                return map[row][column-1] != TILE_TYPES['#']
+            case 1:
+                return map[row][column+1] != TILE_TYPES['#']
+            case 2:
+                return map[row-1][column] != TILE_TYPES['#']
+            case 3:
+                return map[row+1][column] != TILE_TYPES['#']
+            case _:
+                return False
+
+    def turnRight(self):
+        if self.action == 3:
+            self.action = 0
+        elif self.action == 2:
+            self.action = 1
+        else:
+            self.action = (self.action + 2) % 4
+        print(self.action)
 
     def close(self):
         self.env.close()
