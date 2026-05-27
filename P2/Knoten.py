909c96ab60d6837966ac6023b3b93a71a5c09bb5
EmreDogan004 <emre.dogan003@stud.fh-dortmund.de>
2026-05-27 09:15:54 +0000

    PR02 überarbeitet

--- P2/Knoten.py
+++ P2/Knoten.py
@@ -2,14 +2,12 @@ from __future__ import annotations
 
 
 class Knoten:
-    def __init__(self, level, pos_x, pos_y, depth=0):
+    def __init__(self, level, pos_start, depth=1):
         self.level = level
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+        self.pos_start = pos_start
         self.depth = depth
-        self.content = level[pos_x][pos_y]
 
-    direction = [
+    directions = [
         (0, -1),  # links
         (0, 1),  # rechts
         (-1, 0),  # oben
@@ -17,24 +15,28 @@ class Knoten:
     ]
 
     def expand(self) -> list[Knoten]:
-        children = []
-        for direction_x, direction_y in self.direction:
-            next_x = self.pos_x + direction_x
-            next_y = self.pos_y + direction_y
+        neighbours = []
+        pos_start_x, pos_start_y = self.pos_start
+        for direction_x, direction_y in self.directions:
+            next_x = pos_start_x + direction_x
+            next_y = pos_start_y + direction_y
             if 0 <= next_x < len(self.level) and 0 <= next_y < len(self.level[0]):
                 if self.level[next_x][next_y] != "#":
-                    children.append(Knoten(self.level, next_x, next_y, self.depth+1))
-        return children
+                    newLevel = self.adjustLevel((next_x, next_y))
+                    neighbours.append(Knoten(newLevel, (next_x, next_y), self.depth+1))
+        return neighbours
 
+    def adjustLevel(self, new_pos):
+        newLevel = [row.copy() for row in self.level]
+        for x in range(len(newLevel)):
+            for y in range(len(newLevel[x])):
+                if newLevel[x][y] == "P":
+                    newLevel[x][y] = " "
+        new_x, new_y = new_pos
+        newLevel[new_x][new_y] = "P"
+        return newLevel
 
-'''
-    def tiefensuche(self, visited=None):
-        if visited is None:
-            visited = set()
-        visited.add((self.pos_x, self.pos_y))
-        result = [self]
-        for knoten in self.expand():
-            if (knoten.pos_x, knoten.pos_y) not in visited:
-                result.extend(knoten.tiefensuche(visited))
-        return result
-'''
+    def printLevel(self):
+        print(f"Knoten: {self.pos_start}, Tiefe: {self.depth}")
+        for row in self.level:
+            print("[" + " ".join(row) + "]")
