79f6aa38bee3e81337ba2ce677ed2337649e234c
SydneyAntoni <sydney.antoni@xapling.de>
2026-06-05 16:08:03 +0000

    P1

--- PacmanAgent.py
+++ PacmanAgent.py
@@ -1,4 +1,5 @@
 from game_core.Pacman_Environment import Pacman_Environment
+from game_core.config import TILE_TYPES
 
 """
     0: 'left',
@@ -10,7 +11,7 @@ from game_core.Pacman_Environment import Pacman_Environment
 
 class PacmanAgent:
     def __init__(self):
-        # Pacman Environment initialisieren
+        # Pacman Environment initialisieren^
         self.env = Pacman_Environment()
         self.is_running = False
         self.terminated = False
@@ -34,7 +35,20 @@ class PacmanAgent:
 
         if self.is_running and not (self.terminated or self.truncated):
             # TODO P1
-            self.action = 1
+            current_position = self.env.pacman.get_position()
+            if(self.action == 0):
+                if(self.env.view[current_position[0]][current_position[1]-1]==3):
+                    self.action = 2
+            elif(self.action == 1):
+                if(self.env.view[current_position[0]][current_position[1]+1]==3):
+                    self.action = 3
+            elif(self.action==2):
+                if(self.env.view[current_position[0]-1][current_position[1]]==3):
+                    self.action = 1
+            elif(self.action==3):
+                if(self.env.view[current_position[0]+1][current_position[1]]==3):
+                    self.action = 0
+
             observation, reward, self.terminated, self.truncated, self.statistics = self.env.step(self.action)
             if self.terminated or self.truncated:
                 self.is_last_step = True
