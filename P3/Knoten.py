84b2408d95001e24e50529356c2bb30b938517a1
EmreDogan004 <emre.dogan003@stud.fh-dortmund.de>
2026-06-07 19:19:41 +0000

    sys import fix

--- P3/Knoten.py
+++ P3/Knoten.py
@@ -1,10 +1,67 @@
 from __future__ import annotations
 from game_core.config import TILE_TYPES
+import numpy as np
+import sys
 
 
 class Knoten:
-    def __init__(self):
-        pass
+    def __init__(self, pacman_pos_x, pacman_pos_y, level, parent, cost):
+        self.pacman_pos_x: int = pacman_pos_x
+        self.pacman_pos_y: int = pacman_pos_y
+        self.level = level
+        self.parent = parent
+        self.cost = cost
+        self.hash = hash((self.pacman_pos_x, self.pacman_pos_y, tuple(tuple(row) for row in self.level)))
+        self.heuristik = self.set_heuristik()
+        # [[0 for _ in range(4)] for _ in range(4)]
 
     def expand(self) -> list[Knoten]:
-        pass
+        actions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        # Eine Liste/ Array erstellen, in die alle abzuarbeitenden Knoten sind (am anfang nur er selbst)
+        nodes = []
+
+        # Für jede ausführbare Aktion: Die in die Liste/ Array hinzufügen
+        for action_x, action_y in actions:
+            new_pos_x = self.pacman_pos_x + action_x
+            new_pos_y = self.pacman_pos_y + action_y
+            # Schauen ob die aktion ausführbar ist --> Ist das ein "#" oder nicht
+            if self.isValid(new_pos_x, new_pos_y) is True:
+                newLevel = self.move(new_pos_x, new_pos_y)
+                nodes.append(Knoten(new_pos_x, new_pos_y, newLevel, self, self.cost + 1))
+          #      nodes.append(Knoten(new_pos_x, new_pos_y, newLevel, self, self.cost + 1, self.remainingDots))
+
+        return nodes
+
+    def isValid(self, new_pos_x, new_pos_y) -> bool:
+        if (self.level[new_pos_y][new_pos_x] != TILE_TYPES.get('#')):
+            return True
+        return False
+
+    def move(self, new_pos_x, new_pos_y):
+        newLevel = [row.copy() for row in self.level]
+        newLevel[self.pacman_pos_y][self.pacman_pos_x] = TILE_TYPES.get(' ')
+        newLevel[new_pos_y][new_pos_x] = TILE_TYPES.get('P')
+        return newLevel
+
+    def set_heuristik(self) -> int:
+        min_dist = sys.maxsize
+        for i in range(len(self.level)):
+            for j in range(len(self.level[i])):
+                if self.level[i][j] == TILE_TYPES.get("*"):
+                    dist = abs(self.pacman_pos_y - j) + abs(self.pacman_pos_x - i)
+                    if dist < min_dist:
+                        min_dist = dist
+
+        if min_dist == sys.maxsize:
+            return 0
+        return min_dist
+
+    def __eq__(self, other):
+        return (
+            self.pacman_pos_x == other.pacman_pos_x and
+            self.pacman_pos_y == other.pacman_pos_y and
+            np.array_equal(self.level, other.level)
+        )
+
+    def __hash__(self):
+        return self.hash
--- PacmanAgent.py
+++ PacmanAgent.py
@@ -1,6 +1,8 @@
+import sys
+sys.path.append('/app/src/Backend/Student_Files')
 from game_core.Pacman_Environment import Pacman_Environment
-from game_core.config import TILE_TYPES
-
+from P3.Knoten import Knoten
+from P3.Suche import Suche
 """
     0: 'left',
     1: 'right',
@@ -29,30 +31,23 @@ class PacmanAgent:
         return self.env.reset(seed=seed, level=level)
 
     def step(self):
+        observation = None
+
         if self.loesungsknoten is None:
+            print("Ich gehe rein")
             # TODO P3
-            pass
+            startNode = Knoten(self.env.pacman.position_x, self.env.pacman.position_y, self.env.view, None, 0)
+            print("knoten erstellt")
+            suche = Suche(self.a_stern)
+            print("Suche erstellt")
+            self.loesungsknoten = suche.starte_Suchalgorithmus(startNode)
+            print("targetNode gefunden")
+            self.action_path = suche.construct_action_path(self.loesungsknoten)
+            print("Action Path gefunden:")
+            print(self.action_path)
 
         if self.is_running and not (self.terminated or self.truncated):
-            # TODO P1
-            (x, y) = self.env.pacman.get_position()
-            match self.action:
-                case 0:
-                    # if (self.env.view[x][y-1] == TILE_TYPES['#']): # Aufgabe 1.4
-                    if (self.env.view[x-1][y] != TILE_TYPES['#']):  #Zusatzaufgabe
-                        self.action = 2
-                case 1:
-                   #  if (self.env.view[x][y+1] == TILE_TYPES['#']):  # Aufgabe 1.4
-                    if (self.env.view[x+1][y] != TILE_TYPES['#']):  # Zusatzaufgabe
-                        self.action = 3
-                case 2:
-                   #  if (self.env.view[x-1][y] == TILE_TYPES['#']):  # Aufgabe 1.4
-                    if (self.env.view[x][y+1] != TILE_TYPES['#']):  # Zusatzaufgabe
-                        self.action = 1
-                case 3:
-                  #  if (self.env.view[x+1][y] == TILE_TYPES['#']):  # Aufgabe 1.4
-                    if (self.env.view[x][y-1] != TILE_TYPES['#']):  # Zusatzaufgabe
-                        self.action = 0
+            self.move()
 
             observation, reward, self.terminated, self.truncated, self.statistics = self.env.step(self.action)
 
@@ -65,5 +60,52 @@ class PacmanAgent:
 
     def is_game_over(self):
         return self.terminated or self.truncated
+
+    def move(self):
+        # if self.env.bumped_into_wall is True:
+        #     self.change_direction()
+        if self.action_path:
+            self.action = self.action_path.pop(0)
+
+    def change_direction(self):
+        match self.action:
+            case 0: self.action = 2
+            case 1: self.action = 3
+            case 2: self.action = 1
+            case 3: self.action = 0
+
+    def tiefensuche(self, node: Knoten, nodes) -> list[Knoten]:
+        nodes.insert(0, node)
+        return nodes
+
+    def breitensuche(self, node: Knoten, nodes) -> list[Knoten]:
+        nodes.append(node)
+        return nodes
+
+    def ucs(self, node: Knoten, nodes: list[Knoten]) -> list[Knoten]:
+        for i in range(len(nodes)):
+            if node.cost < nodes[i].cost:
+                nodes.insert(i, node)
+                return nodes
+        nodes.append(node)
+        return nodes
+
+    def greedy(self, node: Knoten, nodes: list[Knoten]) -> list[Knoten]:
+        # https://www.datacamp.com/de/tutorial/manhattan-distance
+        for i in range(len(nodes)):
+            if node.heuristik < nodes[i].heuristik:
+                nodes.insert(i, node)
+                return nodes
+        nodes.append(node)
+        return nodes
+
+    def a_stern(self, node: Knoten, nodes: list[Knoten]) -> list[Knoten]:
+        for i in range(len(nodes)):
+            if node.heuristik + node.cost < nodes[i].heuristik + nodes[i].cost:
+                nodes.insert(i, node)
+                return nodes
+        nodes.append(node)
+        return nodes
+
 # track test
 # track test
